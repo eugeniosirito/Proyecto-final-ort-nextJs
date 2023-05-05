1fb748f7f2f02de9337de3c8a54058fbdf59fd32
Eugenio Sirito <eugenio.sirito@gmail.com>
2023-05-05 12:44:51 -0300

    added signIn / signUp component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -13,16 +13,8 @@ export default function Dashboard() {
         <Grid lg={1.5} md={2} sm={3} xs={3} display={'flex'} flexDirection={'column'} sx={{ backgroundColor: 'rgb(35, 48, 68)', height: '100vh' }}>
           <SideMenu />
         </Grid>
-        <Grid lg={10.5} md={10} sm={9} xs={12} sx={{ backgroundColor: 'rgb(27, 38, 53)' }}>
+        <Grid lg={10.5} md={10} sm={9} xs={12}>
           <DashCards />
-          {/* <Grid display={'flex'} xs={12} className={styles.fatherContainer}>
-            <Grid lg={6} xs={12} className={styles.leftContainer}>
-              <SuscribirEstacion />
-            </Grid>
-            <Grid lg={6} xs={12} className={styles.rightContainer}>
-              <ControlPanel />
-            </Grid>
-          </Grid > */}
         </Grid>
         <Footer />
       </Grid>
